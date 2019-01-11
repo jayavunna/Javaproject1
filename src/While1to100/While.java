875cfb9a1fa8710f0a520e702f162ca64a77e6d1
Jaya Vunna <you@example.com>
2019-01-11 14:59:49 +0000

    fjgjlslsd

---
+++ src/While1to100/While.java
@@ -0,0 +1,11 @@
+package While1to100;
+class WhileLoopExample {
+        public static void main(String args[]){
+            int i=100;
+            while(i>1){
+                System.out.println(i);
+                i--;
+            }
+        }
+    }
+
